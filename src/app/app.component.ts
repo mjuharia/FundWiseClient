04dc165ebe8adc2eaea2c3af5435617957192699
mjuharia <movses.juharian@gmail.com>
2025-04-16 21:17:55 -0400

    Added loan application component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
 import { CustomersComponent } from './customers/customers.component';
+import { LoanApplicationComponent } from './loan-application/loan-application.component';
 
 
 Amplify.configure(outputs);
@@ -14,9 +15,11 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent, AmplifyAuthenticatorModule, CustomersComponent],
+  imports: [RouterOutlet, TodosComponent, AmplifyAuthenticatorModule, CustomersComponent, LoanApplicationComponent],
   //imports: [TodosComponent, AmplifyAuthenticatorModule, CustomersComponent],
 })
+
+
 export class AppComponent {
   title = 'amplify-angular-template';
     
---
+++ src/app/loan-application/loan-application.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-loan-application',
+  templateUrl: './loan-application.component.html',
+  styleUrls: ['./loan-application.component.css'],
+  imports: [ReactiveFormsModule],
+  standalone: true
+})
+
+
+export class LoanApplicationComponent {
+  loanForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loanForm = this.fb.group({
+      loanAmount: ['', Validators.required],
+      loanTerm: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      dob: ['', Validators.required],
+      phone: ['', Validators.required],
+      income: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loanForm.valid) {
+      console.log(this.loanForm.value);
+    }
+  }
+}
